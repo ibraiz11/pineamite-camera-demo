c93e3cf95aae2878753e2e2dde1708dda7f7bfe3
Ibraiz <ibraizqazi@gmail.com>
2025-11-15 23:00:46 -0800

    Initial Commit

---
+++ pineamite-camera/pineamite_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  pineamite_cameraApp.swift
+//  pineamite-camera
+//
+//  Created by Ibraiz Qazi on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct pineamite_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pineamite-cameraTests/pineamite_cameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  pineamite_cameraTests.swift
+//  pineamite-cameraTests
+//
+//  Created by Ibraiz Qazi on 11/15/25.
+//
+
+import Testing
+@testable import pineamite_camera
+
+struct pineamite_cameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
